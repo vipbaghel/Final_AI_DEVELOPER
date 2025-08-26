42b8d9cf553825a15af600f6cd3ba3d95260e35a
vipin baghel <146691079+vipbaghel@users.noreply.github.com>
2025-08-26 19:25:15 +0530

    main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'remixicon/fonts/remixicon.css'
+import hljs from "highlight.js";
+import "highlight.js/styles/nord.css";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
