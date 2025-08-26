213e38c2cd11de9375f7e37830c84e19a0e9f4b2
vipin baghel <146691079+vipbaghel@users.noreply.github.com>
2025-08-26 20:25:07 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
-
-
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        "Authorization": `Bearer ${localStorage.getItem('token')}`
-    }
-})
+  baseURL: import.meta.env.VITE_API_URL,
+});
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-
-export default axiosInstance;   
+export default axiosInstance;
