8af194df2e1c966e4c5216ced99bf80110b8dd12
vipin baghel <146691079+vipbaghel@users.noreply.github.com>
2025-08-26 19:48:31 +0530

    webContainer.js

--- frontend/src/config/webContainer.js
+++ frontend/src/config/webContainer.js
@@ -1,12 +1,10 @@
-import webcontainer from "../config/WebContainer";
-
+import { WebContainer } from "@webcontainer/api";
 
 let webContainerInstance = null;
 
-
 export const getWebContainer = async () => {
-    if (webContainerInstance === null) {
-        webContainerInstance = await WebContainer.boot();
-    }
-    return webContainerInstance;
-}
+  if (webContainerInstance === null) {
+    webContainerInstance = await WebContainer.boot();
+  }
+  return webContainerInstance;
+};
